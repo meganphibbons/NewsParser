c8c03aadc4ff3fb8e52ba21af7012f25a8fb0ff6
Megan Phibbons <megan.phibbons@gmail.com>
2021-04-20 15:53:09 -0400

    Implement priority queue. Now, all that needs to be done is removing terms like 'and' 'or' 'but' etc. and returning the top keywords

--- KeywordGenerator.ml
+++ KeywordGenerator.ml
@@ -14,11 +14,35 @@ let rec populate_table table = function
 ;;
 
 let rec print_table table = 
-  Hashtbl.iter (fun key value -> Printf.printf "%s -> %d\n" key value) table;;
+  Hashtbl.iter (fun key value -> Printf.printf "%s -> %d\n" key value) table
+;;
+
+let rec print_pq = function 
+    | [] -> ()
+    | (key, value)::l -> print_string key; print_string "-> "; print_string (string_of_int value); print_string "\n" ; print_pq l
+;;
+
+let rec add_to_pq (key, value) pq = 
+  match pq with  
+  | [] -> (key, value)::[]
+  | (hkey, hval)::t when value > hval -> (key, value)::pq
+  | h::t -> h::(add_to_pq (key, value) t)
+;;
+
+let rec make_pq pq = function 
+  | [] -> pq
+  | (k, v)::t -> make_pq (add_to_pq (k, v) pq) t
+;;
+
+let make_pair_list table = 
+  Hashtbl.fold (fun k v acc -> (k, v) :: acc) table []
+;;
 
 let generate_keywords article_text = 
   let table = Hashtbl.create 500 in
   (* val my_hash : (string, int) Hashtbl.t;; map from string to num occurrences *)
   let word_list = (Str.split whitespace (Str.global_replace (Str.regexp "\.") " " article_text)) in 
-  print_table (populate_table table word_list)
+  let table = (populate_table table word_list) in 
+  let pair_list = (make_pair_list table) in
+  print_pq (make_pq [] pair_list)
 ;;
