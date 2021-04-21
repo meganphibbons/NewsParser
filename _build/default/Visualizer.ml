772842c218f34f944b8f306447d11289441e9927
Megan Phibbons <megan.phibbons@gmail.com>
2021-04-21 14:23:25 -0400

    Add back in the Utility file

--- _build/default/Visualizer.ml
+++ _build/default/Visualizer.ml
@@ -1,19 +1,4 @@
-(* type 'a tree = Empty | Leaf of 'a | Node of 'a tree * 'a * 'a tree;;
 
-
-
-let t1 = Node (
-              Node (
-                    Leaf "0", 
-                    "1", 
-                    Leaf "2"), 
-              "3", 
-              Node (
-                    Leaf "4", 
-                    "5", 
-                    Leaf "6"
-              ) 
-         );; *)
 open TreeSamples
 
 let to_dot_node (t: 'a tree) : string = match t with 
