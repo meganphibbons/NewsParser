29a009f6484596e20ebaf497f31d99b2c9ddca88
dorianbarber <thedorianbarber@gmail.com>
2021-04-21 18:40:29 -0400

    tree builder function

--- NewsCrawler.ml
+++ NewsCrawler.ml
@@ -2,6 +2,7 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 open Parser
+open Tree
 
 let base_string = "https://hacker-news.firebaseio.com/v0/item/"
 let max_item_string = "https://hacker-news.firebaseio.com/v0/maxitem.json"
@@ -24,7 +25,15 @@ let rec find_parent_id id =
   let curr_json = parse (curr) in 
   if curr_json.post_type = "comment" then find_parent_id (curr_json.parent) else curr_json.id
 
+let rec build_json_tree id = 
+  let curr = Lwt_main.run (get_item_from_id id) in 
+  let curr_json = parse (curr) in
+  if curr_json.descendants <> None then Node {value = curr_json ; children = List.map build_json_tree curr_json.children}
+  else Leaf curr_json
+
+
 let () =
   let res = Lwt_main.run (get_item_from_id 8863) and
   rando = Lwt_main.run (get_random_item) in
   print_endline ("Received \n" ^ res ^ (Str.replace_first r "\\1" "hello world") ^ string_of_int(rando))
+
---
+++ Tree.ml
@@ -0,0 +1,13 @@
+open Parser
+
+type 'a tree = 
+  | Leaf of 'a
+  | Node of 'a node
+
+and 'a node = {
+  value: 'a;
+  children: 'a tree list  
+}
+
+
+
